4de05559d6167c75e356bb34c42a24d76b5079a2
Andrew Thompson <Andrew_Thompson@rapid7.com>
2015-10-22 11:57:34 -0400

    CORE-1249 Rubocop fixes

--- lib/elasticsearch/drain.rb
+++ lib/elasticsearch/drain.rb
@@ -22,7 +22,8 @@ module Elasticsearch
 
     # Sets up the Elasticsearch client
     #
-    # @option [String] :hosts ('localhost:9200') The Elasticsearch hosts to connect to
+    # @option [String] :hosts ('localhost:9200') The Elasticsearch hosts
+    #   to connect to
     # @return [Elasticsearch::Transport::Client] Elasticsearch transport client
     def initialize(hosts: 'localhost:9200', asg:, region:)
       @hosts = hosts
@@ -32,7 +33,7 @@ module Elasticsearch
         hosts: hosts,
         retry_on_failure: true,
         log: true,
-        logger: Logger.new('es_client.log', 10, 1024000)
+        logger: Logger.new('es_client.log', 10, 1_024_000)
       )
     end
 
@@ -42,10 +43,59 @@ module Elasticsearch
     def nodes
       Nodes.new.nodes
     end
+
+    def drain
+      # if [ -z "$ASG_NAME" ]; then
+      #     echo "ASG_NAME is missing"
+      #     usage
+      #     exit 1
+      # fi
+      #
+      # if [ -z "$REGION" ]; then
+      #     echo "REGION is missing"
+      #     usage
+      #     exit 1
+      # fi
+      #
+      # if [ -z "$CLUSTER_HOST" ]; then
+      #     echo "CLUSTER_HOST is missing"
+      #     usage
+      #     exit 1
+      # fi
+      #
+      # cluster_health
+      # get_nodes_in_asg
+      # echo "Found nodes in ASG: $INSTANCES"
+      #
+      # cluster_health
+      # set_asg_min_size
+      #
+      # for i in $INSTANCES; do
+      #     get_node_ipaddress
+      #     NODE=$IP
+      #     NODES+="${NODE},"
+      # done
+      # NODES=${NODES%?}
+      #
+      # echo "Sleeping 1 minute before starting"
+      # sleep 60
+      # echo "Draining data from $NODES"
+      #
+      # for i in $INSTANCES; do
+      #     get_node_ipaddress
+      #     INSTANCE_ID=$i
+      #     NODE=$IP
+      #     echo "Removing $NODE from ES cluster and $ASG_NAME AutoScalingGroup"
+      #     cluster_health
+      #     remove_node_from_cluster
+      #     cluster_health
+      #     echo "Sleeping for 1 minute before removing the next node"
+      #     sleep 60
+      # done
+    end
   end
 end
 
 require_relative 'drain/autoscaling'
 require_relative 'drain/version'
 require_relative 'drain/nodes'
-
