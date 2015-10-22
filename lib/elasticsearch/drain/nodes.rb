4a1d434f98ab2e8efefecd38fe42de4df228f662
Andrew Thompson <Andrew_Thompson@rapid7.com>
2015-10-22 11:58:42 -0400

    CORE-1249 Fixes super call in Nodes class

--- lib/elasticsearch/drain/nodes.rb
+++ lib/elasticsearch/drain/nodes.rb
@@ -10,7 +10,7 @@ module Elasticsearch
       attr_reader :info
 
       def initialize(_)
-        super(hosts: hosts)
+        super
         @info = client.nodes.info metric: '_all'
         @stats = client.nodes.stats metric: '_all'
       end
