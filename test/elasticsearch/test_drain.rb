67c8b6da0c5f2eae93d99e430fe49ca79103a651
Andrew Thompson <Andrew_Thompson@rapid7.com>
2015-10-26 12:13:35 -0400

    CORE-1249 Removing unneeded stuff...

--- test/elasticsearch/test_drain.rb
+++ test/elasticsearch/test_drain.rb
@@ -17,11 +17,6 @@ class TestDrain < Minitest::Test
     assert_respond_to @drain, :hosts
   end
 
-  # def test_nodes_in_recovery
-  #   skip("NYI")
-  #   assert_respond_to @drain.recovery :each
-  # end
-
   def test_has_asg
     assert_equal 'esuilogs-razor-d0prod-r01-v000', @drain.asg.asg
   end
