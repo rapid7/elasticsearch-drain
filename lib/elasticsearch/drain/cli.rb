f1b310db21389be9ab24060e0f9b15586c92e472
Andrew Thompson <Andrew_Thompson@rapid7.com>
2015-11-03 12:26:52 -0500

    CORE-1249 Make api read better

--- lib/elasticsearch/drain/cli.rb
+++ lib/elasticsearch/drain/cli.rb
@@ -4,11 +4,11 @@ module Elasticsearch
     class CLI < ::Thor
       package_name :elasticsearch
 
-      desc 'drain', 'Drain all documents from all nodes in an EC2 AutoScaling Group'
+      desc 'asg', 'Drain all documents from all nodes in an EC2 AutoScaling Group'
       option :host, default: 'localhost:9200'
       option :asg, required: true
       option :region, required: true
-      def drain
+      def asg
         drainer = Elasticsearch::Drain.new(options[:host],
                                            options[:asg],
                                            options[:region])
